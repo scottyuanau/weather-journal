77bb5af8387a70c5a921abf8e33fd5b130fd7f29
Scott Yuan <scoworthyuan@gmail.com>
2022-12-05 21:40:45 +1100

    added weather api, returned coordinates & weather data. needs to fix lat & lon undefined issue in next version.

--- website/js/app.js
+++ website/js/app.js
@@ -1,10 +1,73 @@
-let baseURL = 'http://api.animalinfo.org/data/?animal='
-let apiKey = '&appid=9f15e45060...';
+/*Example API Call for Weather
+https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+*/
+
+/*
+//Geocode Zip API Call
+https://api.openweathermap.org/geo/1.0/zip?zip=71601&appid=69460dd362bf8c5d994887599f201a80
+
+*/
+
+let weatherURL = 'https://api.openweathermap.org/data/2.5/weather?';
+let zipURL = 'https://api.openweathermap.org/geo/1.0/zip?zip=';
+let apiKey = '&appid=69460dd362bf8c5d994887599f201a80&units=imperial';
 
 
 
-document.querySelector('.generate').addEventListener('click',()=>{
-    let feelings = document.querySelector('.feelings').value;
-    let zip = document.querySelector('#zip').value;
-    console.log(feelings,zip);
+document.querySelector('.generate').addEventListener('click',performAction);
+let lat;
+let lon; //needs to do something here, first time is returning undefined, second time is ok.
+
+function performAction() {
+        let feelings = document.querySelector('.feelings').value;
+        let zip = document.querySelector('#zip').value;
+
+        getCoordinates(zipURL,apiKey,zip)
+        .then(getTemperature(lat, lon,weatherURL,apiKey));
+
+
+
+}
+
+//find coordinates
+    const getCoordinates = async (url,key,zip)=>{
+        const res = await fetch(url+zip+key);
+        try {
+            const data = await res.json();
+            lat = data.lat;
+            lon = data.lon;
+            return data;
+        } catch(error) {
+            console.log('error',error);
+        }
+
+    };
+//find temperature
+    const getTemperature = async (lat,lon,url,key)=>{
+        const res = await fetch(url+'lat='+lat+'&lon='+lon+key);
+        try {
+            const data = await res.json();
+            console.log(data);
+            return data;
+        } catch(error) {
+            console.log('error',error);
+        }
+    };
+
+
+
+
+
+//placeholder effect
+document.querySelector('.feelings').addEventListener('focus',()=>{
+    document.querySelector('.feelings').classList.add('activeplace-holder-feeling');
+});
+document.querySelector('.feelings').addEventListener('blur',()=>{
+    document.querySelector('.feelings').classList.remove('activeplace-holder-feeling');
+});
+document.querySelector('#zip').addEventListener('focus',()=>{
+    document.querySelector('#zip').classList.add('activeplace-holder-zip');
+});
+document.querySelector('#zip').addEventListener('blur',()=>{
+    document.querySelector('#zip').classList.remove('activeplace-holder-zip');
 });
